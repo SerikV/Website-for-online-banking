7b4408629daaadf07e97c6aa7b0f714b01c0791e
admin <admin@admins-Mac-mini.local>
2025-02-27 16:45:32 +0200

    billing update

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -18,9 +18,7 @@ const Billing = () => (
         invoicing
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
-        aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, cumque deleniti? Assumenda, consequuntur exercitationem aperiam soluta delectus sint minima itaque ducimus ullam officiis?
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
